28f19b8eb05a31c266d16837ca627a80d17330c5
igor.araujo <igor.dearaujo@live.com>
2020-06-12 02:14:00 -0300

    migrating to work computer

--- utils/csv_handler.py
+++ utils/csv_handler.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import re
 
+class Person:
+  def __init__(self, coords, id_pess):
+    self.coords = coords
+    self.id_pess = id_pess
+
+#   def myfunc(self):
+#     print("Hello my name is " + self.name)
 
 def read_csv():
     
@@ -12,7 +19,7 @@ def read_csv():
     # data = pd.read_csv('OD_2017_1.csv')
     # data.head()
 
-    cols_list = ["ZONA", "ID_PESS"]
+    cols_list = ["ZONA", "CO_DOM_X", "CO_DOM_Y", "ID_PESS", ]
     print(cols_list)
     csv_data = pd.read_csv("C:/Users/Igor/Documents/GitHub/ep1-aed2/utils/OD_2017.csv", sep=',', usecols=cols_list)
     print(csv_data['ZONA'])
@@ -32,13 +39,14 @@ def construct_arrays(columns):
 
     ids = columns.ID_PESS.tolist()
 
-    # for item in columns:
-    #     ids[item] = columns['ID_PESS']
+    ids = ids.drop_duplicates(keep=First)
+
+    for item in columns:
         
     #     #Ve se a coluna é alguma da constante
-    #     # coord_regex = re.search('^CO_*_X$', )
-    #     # if column == (coord_regex):
-    #     #     coords[[item]].append()
+        coord_regex = re.search('^CO_*_X$', )
+        if column == (coord_regex):
+            coords[[item]].append()
 
     ids = remove_duplicates(ids, 'array')
     coord = remove_duplicates(coords, 'array of arrays')
